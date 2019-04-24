702c1fbaa7266ade60c8edb22550ff37f5a485ad
Death_Kiss <xca1983@163.com>
2019-04-24 14:16:50 +0800

    跨平台 Linux

--- Client/ClientMain.cpp
+++ Client/ClientMain.cpp
@@ -1,9 +1,22 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN
+#ifdef _WIN32
 #include<Windows.h>
 #include<WinSock2.h>
+#else
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<string.h>
+#define SOCKET int
+#define INVALID_SOCKET  (SOCKET)(~0)
+#define SOCKET_ERROR            (-1)
+
+#endif // _WIN32
+
+
 #include<iostream>
 #include<thread>
+#include<stdio.h>
 using namespace std;
 enum MessageType
 {
@@ -22,47 +35,60 @@ void cmdThread(SOCKET s)
 	while (true)
 	{
 		DataPackage recvdata;
-		cout << "请输入用户名:" << endl;
+		cout << "please enter username:" << endl;
 		cin >> recvdata._name;
-		cout << "请输入密码:" << endl;
+		cout << "please enter password:" << endl;
 		cin >> recvdata._password;
 		recvdata.dataLen = sizeof(DataPackage);
 		recvdata.msType = Login;
 		//4.向服务器发送消息
 		if (SOCKET_ERROR == send(s, (const char*)&recvdata, sizeof(DataPackage), 0))
 		{
-			cout << "向服务器发送消息失败" << endl;
+			cout << "send message to server failed" << endl;
 		}//根据输入项服务器发送消息
 		else
 		{
-			cout << "向服务器发送成功消息" << endl;
+			cout << "send message to server success" << endl;
 		}
 	}
 }
-int main() {
+int main()
+{
+#ifdef _WIN32
 	//1.Winsock服务的初始化 WSAStartup
 	WORD version = MAKEWORD(2, 2);
 	WSADATA data;
 	WSAStartup(version, &data);
+#endif // _WIN32
+
+
 	//2.创建Socket
 	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (INVALID_SOCKET == clientSocket) {
-		cout << "客户端创建Socket失败!" << endl;
+	if (INVALID_SOCKET == clientSocket)
+	{
+		cout << "Creat socket failed!" << endl;
 	}
 	else
 	{
-		cout << "客户端创建Socket成功! 服务端Socket:" << clientSocket<< endl;
+		cout << "Creat socket success!:" << clientSocket << endl;
 	}
 	//3.连接服务器服务器
 	sockaddr_in serverAddr = {};
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(4567);
+#ifdef _WIN32
+	//serverAddr.sin_addr.S_un.S_addr = inet_addr("129.204.40.157");
 	serverAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-	if (SOCKET_ERROR == connect(clientSocket, (sockaddr*)& serverAddr, sizeof(sockaddr))) {
-		cout << "连接服务器失败!" << endl;
+#else
+	serverAddr.sin_addr.s_addr = inet_addr("129.204.40.157");
+#endif // _WIN32
+	if (SOCKET_ERROR == connect(clientSocket, (sockaddr*)& serverAddr, sizeof(sockaddr)))
+	{
+		cout << "connect server failed" << endl;
 	}
-	else {
-		cout << "连接服务器成功!" << endl;
+	else
+	{
+		cout << "connect server success!" << endl;
 
 	}
 
@@ -71,37 +97,40 @@ int main() {
 	//int recvlen = recv(clientSocket, msBuff, 128, 0);
 	//cout << "服务器消息:" << msBuff << endl;
 	thread t1(cmdThread, clientSocket);//启动一个线程
-	while (true) {
+	while (true)
+	{
 
 		fd_set readfds; FD_ZERO(&readfds);
 		timeval times = { 1,0 };
 		FD_SET(clientSocket, &readfds);
-		int ret=select(clientSocket + 1, &readfds, 0, 0, &times);
+		int ret = select(clientSocket + 1, &readfds, 0, 0, &times);
 		if (ret < 0)
 		{
-			cout << "与服务器断开连接" << endl;
+			cout << "dis connect server" << endl;
 			break;
 		}
 		if (FD_ISSET(clientSocket, &readfds))
 		{
 			//5.接收服务器信息		
 			int recvlen = recv(clientSocket, msbuff2, 128, 0);
-			cout << "服务器返回长度:" << recvlen << endl;
+			cout << "return server len:" << recvlen << endl;
 			if (recvlen > 0)
 			{
-				cout << "收到来自服务器的消息:" << msbuff2 << endl;
+				cout << "recv server message:" << msbuff2 << endl;
 			}
 			else
 			{
-				cout << "服务器响应失败" << endl;
+				cout << "server no return" << endl;
 			}
 		}
 		//cout << "主线程空闲时处理其他业务" << endl;
 	}
+#ifdef _WIN32
 	//6.关闭Socket
 	closesocket(clientSocket);
 	//7.Winsock服务的注销 WSACleanup()
 	WSACleanup();
+#endif // _WIN32
 	cout << "Hello Client!" << endl;
 	system("pause");
 	return EXIT_SUCCESS;
--- SocketServer/SocketServerMain.cpp
+++ SocketServer/SocketServerMain.cpp
@@ -1,9 +1,21 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN
+#ifdef _WIN32
 #include<Windows.h>
-#include<iostream>
 #include<WinSock2.h>
-using namespace std;
+#else
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<string.h>
+#define SOCKET int
+#define INVALID_SOCKET  (SOCKET)(~0)
+#define SOCKET_ERROR            (-1)
+#endif // _WIN32
+#include<iostream>
+#include<thread>
+//using namespace std;
+using std::cout;
+using std::endl;
 enum MessageType
 {
 	Login,
@@ -41,7 +53,7 @@ int RecvMessage(SOCKET sock)
 				{
 					//send(clientSocket, "登录成功!", 128, 0);
 					send(sock, "登录成功!", 128, 0);
-					cout <<"向客户端"<<sock<< "发送登录成功消息!" << endl;
+					cout << "向客户端" << sock << "发送登录成功消息!" << endl;
 				}
 				else
 				{
@@ -66,41 +78,52 @@ int RecvMessage(SOCKET sock)
 	}
 }
 
-int main() {
+int main()
+{
+#ifdef _WIN32
 	//1.Winsock服务的初始化 WSAStartup
-	WORD ver = MAKEWORD(2, 2);
+	WORD version = MAKEWORD(2, 2);
 	WSADATA data;
-	WSAStartup(ver, &data);//调用了windows的动态库，需加入库文件 ws2_32.lib  初始化Ws2_32.dll的函数
+	WSAStartup(version, &data);
+#endif // _WIN32
+
 	//2.创建Socket
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);//创建Socket 参数1 socket
 	if (SOCKET_ERROR == sock)
 	{
-		cout << "服务端创建Socket失败!" << endl;
+		cout << "Creat Socket failed!" << endl;
 	}
 	else
 	{
-		cout << "服务端创建Socket成功!Socket:" << sock << endl;
+		cout << "Creat Socket success!:" << sock << endl;
 	}
 	//3.绑定用于接收客户端连接的网络端口
 	sockaddr_in _sin = {};//指定地址
 	_sin.sin_family = AF_INET;//地址采用的协议
 	_sin.sin_port = htons(4567);//地址端口
+#ifdef _WIN32
 	_sin.sin_addr.S_un.S_addr = INADDR_ANY;//地址
-
-	if (SOCKET_ERROR == bind(sock, (sockaddr*)&_sin, sizeof(sockaddr_in))) {//绑定端口
-		std::cout << "服务端绑定端口失败!" << endl;
+#else
+	_sin.sin_addr.s_addr = INADDR_ANY; //inet_addr("129.204.40.157");
+#endif // _WIN32
+	if (SOCKET_ERROR == bind(sock, (sockaddr*)&_sin, sizeof(sockaddr_in)))
+	{//绑定端口
+		cout << "bind failed!" << endl;
 	}
-	else {
-		std::cout << "服务端绑定端口成功!" << endl;
+	else
+	{
+		cout << "bind success!" << endl;
 	}
 	//4.监听网络端口
-	if (SOCKET_ERROR == listen(sock, 5)) {
-		std::cout << "服务端监听端口失败!" << endl;
+	if (SOCKET_ERROR == listen(sock, 5))
+	{
+		std::cout << "listen failed!" << endl;
 	}
-	else {
-		std::cout << "服务端监听端口成功!" << endl;
+	else
+	{
+		std::cout << "listen success!" << endl;
 	}
-	
+
 
 	timeval times = { 1,0 };
 	SOCKET socks[4];
@@ -108,7 +131,8 @@ int main() {
 	int totalSock = 1;
 
 
-	while (true) {
+	while (true)
+	{
 		//cout << "Socket总数:" << totalSock << endl;
 		fd_set readfds; FD_ZERO(&readfds);//可读集合
 		fd_set writefds; FD_ZERO(&writefds);//可写集合
@@ -120,10 +144,10 @@ int main() {
 			FD_SET(socks[i], &writefds);//将监听Socket加入可写集合(服务端Socket)
 			FD_SET(socks[i], &exceptfds);//将监听Socket加入异常集合(服务端Socket)
 		}
-		int ret = select(sock + 1, &readfds, &writefds, &exceptfds,&times);
+		int ret = select(sock + 1, &readfds, &writefds, &exceptfds, &times);
 		if (ret < 0)
 		{
-			cout << "select 返回值小于0 " << endl;
+			cout << "select failed " << endl;
 
 			break;
 		}
@@ -138,7 +162,12 @@ int main() {
 					sockaddr_in clientAddr = {};//远程客户端地址
 					int clientAddrLen = sizeof(sockaddr_in);
 					SOCKET clientSocket = INVALID_SOCKET;
+#ifdef _WIN32
 					clientSocket = accept(sock, (sockaddr*)&clientAddr, &clientAddrLen);//参数1 listen函数用到的socket,参数2 远程客户端地址 参数3 远程客户端长度 返回远程客户端Socket
+#else
+					clientSocket = accept(sock, (sockaddr*)&clientAddr, (socklen_t*)& clientAddrLen);//参数1 listen函数用到的socket,参数2 远程客户端地址 参数3 远程客户端长度 返回远程客户端Socket
+#endif // _WIN32
+
 					if (SOCKET_ERROR == clientSocket)
 					{
 						std::cout << "服务端接收客户端链接失败 ,无效的客户端Socket!" << endl;
@@ -146,9 +175,9 @@ int main() {
 					}
 					else
 					{
-						std::cout << "服务端接收客户端链接成功成功!Socket" <<clientSocket<< endl;
+						std::cout << "服务端接收客户端链接成功成功!Socket" << clientSocket << endl;
 						std::cout << "新客户端加入成功,IP地址为:!" << inet_ntoa(clientAddr.sin_addr) << endl;
-					/*	char msbuff[128];
+						/*	char msbuff[128];
 						recv(clientSocket, msbuff, 128, 0);
 						cout << "收到客户端消息" << msbuff << endl;*/
 						//send(clientSocket, "I'm server!", 128, 0);
@@ -157,11 +186,12 @@ int main() {
 							send(socks[i], "新用户加入", 128, 0);
 						}
 						socks[totalSock++] = clientSocket;
-						cout <<"总Socket数:"<< totalSock << endl;
+						cout << "总Socket数:" << totalSock << endl;
 					}
 				}
-				else{	//6.接收某个客户端消息
-					
+				else
+				{	//6.接收某个客户端消息
+
 					if (RecvMessage(socks[i]) == -1)
 					{
 						/*for (int i = 0; i < totalSock; ++i)
@@ -172,12 +202,18 @@ int main() {
 					}
 				}
 			}
-		}	
+		}
 	}
 
 	//8.关闭Socket
 	//closesocket(clientSocket);
-	WSACleanup();//Winsock服务的注销 WSAStartup
+#ifdef _WIN32
+	//closesocket(clientSocket);
+	WSACleanup();
+#else
+	close(sock);
+#endif // _WIN32
+
 
 	cout << "Hello Socket!" << endl;
 	system("pause");
